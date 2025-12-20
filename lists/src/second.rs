1f776a1a7c1d3662625a291fc960e4db63178e5e
Jonathan Lin <jlin@winteroftext.ca>
2025-12-20 15:07:54 -0800

    write tests for into_iter

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -131,4 +131,17 @@ mod test {
         assert_eq!(list.pop(), Some(42));
     }
 
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+
+    }
+
 }
