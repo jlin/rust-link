0ad6383adb40e684d2caa07b372ffa3e433c634b
Jonathan Lin <jlin@winteroftext.ca>
2025-12-20 17:38:51 -0800

    iter_mut, ownership, copy, take() and some other stuff

--- lists/src/second.rs
+++ lists/src/second.rs
@@ -9,6 +9,10 @@ pub struct IntoIter<T>(List<T>);
 pub struct Iter<'a, T>{
     next: Option<&'a Node<T>>,
 }
+
+pub struct IterMut<'a, T>{
+    next: Option<&'a mut Node<T>>,
+}
 // type alias!!
 type Link<T> = Option<Box<Node<T>>>;
 
@@ -24,6 +28,10 @@ impl<T> List<T>{
 
     pub fn iter(&self) -> Iter<'_, T> {
         Iter { next: self.head.as_deref()}
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut { next: self.head.as_deref_mut()}
     }
 }
 
@@ -83,6 +91,18 @@ impl<'a, T> Iterator for Iter<'a, T>{
         })
     }
 }
+
+impl<'a, T> Iterator for IterMut<'a, T>{
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
+}
+
 
 
 // crazy drop to avoid recursion
@@ -178,5 +198,18 @@ mod test {
         assert_eq!(iter.next(), Some(&1));
 
     }
+
+     #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+
+    }
+
 
 }
